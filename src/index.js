14cb39caf2dec5489355490539c6b4fda12d0415
Kim Seungha <seungha.me@gmail.com>
2018-11-06 10:51:31 +0900

    로그인하면 게시물 목록 페이지로 이동하는 기능

--- src/index.js
+++ src/index.js
@@ -35,23 +35,73 @@ const rootEl = document.querySelector('.root')
 // 6. 템플릿을 문서에 삽입
 
 async function drawLoginForm() {
+  // 1. 템플릿 복사
+  const frag = document.importNode(templates.loginForm, true)
 
+  // 2. 요소 선택
+  const formEl = frag.querySelector('.login-form')
+
+  // 3. 필요한 데이터 불러오기 - 필요없음
+  // 4. 내용 채우기 - 필요없음
+  // 5. 이벤트 리스너 등록하기
+  formEl.addEventListener('submit', async e => {
+    e.preventDefault()
+    const username = e.target.elements.username.value
+    const password = e.target.elements.password.value
+
+    const res = await api.post('/users/login', {
+      username,
+      password
+    })
+
+    localStorage.setItem('token', res.data.token)
+    drawPostList()
+  })
+
+  // 6. 템플릿을 문서에 삽입
+  rootEl.textContent = ''
+  rootEl.appendChild(frag)
 }
 
 async function drawPostList() {
+  // 1. 템플릿 복사
+  const frag = document.importNode(templates.postList, true)
 
+  // 2. 요소 선택
+  // 3. 필요한 데이터 불러오기
+  // 4. 내용 채우기
+  // 5. 이벤트 리스너 등록하기
+
+  // 6. 템플릿을 문서에 삽입
+  rootEl.textContent = ''
+  rootEl.appendChild(frag)
 }
 
 async function drawPostDetail(postId) {
-
+  // 1. 템플릿 복사
+  // 2. 요소 선택
+  // 3. 필요한 데이터 불러오기
+  // 4. 내용 채우기
+  // 5. 이벤트 리스너 등록하기
+  // 6. 템플릿을 문서에 삽입
 }
 
 async function drawNewPostForm() {
-
+  // 1. 템플릿 복사
+  // 2. 요소 선택
+  // 3. 필요한 데이터 불러오기
+  // 4. 내용 채우기
+  // 5. 이벤트 리스너 등록하기
+  // 6. 템플릿을 문서에 삽입
 }
 
 async function drawEditPostForm(postId) {
-
+  // 1. 템플릿 복사
+  // 2. 요소 선택
+  // 3. 필요한 데이터 불러오기
+  // 4. 내용 채우기
+  // 5. 이벤트 리스너 등록하기
+  // 6. 템플릿을 문서에 삽입
 }
 
 // 페이지 로드 시 그릴 화면 설정
