135a4a7e571866ec8b758540cfc23d3366f453d7
Kim Seungha <seungha.me@gmail.com>
2018-11-06 10:45:46 +0900

    initial

---
+++ src/index.js
@@ -0,0 +1,62 @@
+import '@babel/polyfill'
+
+import axios from 'axios'
+
+const api = axios.create({
+  baseURL: process.env.API_URL
+})
+
+api.interceptors.request.use(function (config) {
+  const token = localStorage.getItem('token')
+  if (token) {
+    config.headers = config.headers || {}
+    config.headers['Authorization'] = 'Bearer ' + token
+  }
+  return config
+});
+
+const templates = {
+  loginForm: document.querySelector('#login-form').content,
+  postList: document.querySelector('#post-list').content,
+  postItem: document.querySelector('#post-item').content,
+  postForm: document.querySelector('#post-form').content,
+  postDetail: document.querySelector('#post-detail').content,
+  commentItem: document.querySelector('#comment-item').content,
+}
+
+const rootEl = document.querySelector('.root')
+
+// 페이지 그리는 함수 작성 순서
+// 1. 템플릿 복사
+// 2. 요소 선택
+// 3. 필요한 데이터 불러오기
+// 4. 내용 채우기
+// 5. 이벤트 리스너 등록하기
+// 6. 템플릿을 문서에 삽입
+
+async function drawLoginForm() {
+
+}
+
+async function drawPostList() {
+
+}
+
+async function drawPostDetail(postId) {
+
+}
+
+async function drawNewPostForm() {
+
+}
+
+async function drawEditPostForm(postId) {
+
+}
+
+// 페이지 로드 시 그릴 화면 설정
+if (localStorage.getItem('token')) {
+  drawPostList()
+} else {
+  drawLoginForm()
+}
