4e7b2b9dc26f2349c4d3ffc382bad7de89da4799
Kim Seungha <seungha.me@gmail.com>
2018-11-06 17:11:57 +0900

    새 글 쓰기 기능

--- src/index.js
+++ src/index.js
@@ -71,6 +71,7 @@ async function drawPostList() {
 
   // 2. 요소 선택
   const listEl = frag.querySelector('.post-list')
+  const createEl = frag.querySelector('.create')
 
   // 3. 필요한 데이터 불러오기
   const {data: postList} = await api.get('/posts?_expand=user')
@@ -97,6 +98,9 @@ async function drawPostList() {
   }
 
   // 5. 이벤트 리스너 등록하기
+  createEl.addEventListener('click', e => {
+    drawNewPostForm()
+  })
 
   // 6. 템플릿을 문서에 삽입
   rootEl.textContent = ''
@@ -183,11 +187,33 @@ async function drawPostDetail(postId) {
 
 async function drawNewPostForm() {
   // 1. 템플릿 복사
+  const frag = document.importNode(templates.postForm, true)
+
   // 2. 요소 선택
+  const formEl = frag.querySelector('.post-form')
+  const backEl = frag.querySelector('.back')
+
   // 3. 필요한 데이터 불러오기
   // 4. 내용 채우기
   // 5. 이벤트 리스너 등록하기
+  formEl.addEventListener('submit', async e => {
+    e.preventDefault()
+    const title = e.target.elements.title.value
+    const body = e.target.elements.body.value
+    await api.post('/posts', {
+      title,
+      body
+    })
+    drawPostList()
+  })
+  backEl.addEventListener('click', e => {
+    e.preventdefault()
+    drawPostList()
+  })
+
   // 6. 템플릿을 문서에 삽입
+  rootEl.textContent = ''
+  rootEl.appendChild(frag)
 }
 
 async function drawEditPostForm(postId) {
