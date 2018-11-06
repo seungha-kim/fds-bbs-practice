02d838c8e03dc80347c462600b8ad2e1dd987fb4
Kim Seungha <seungha.me@gmail.com>
2018-11-06 16:23:03 +0900

    댓글 표시 기능

--- src/index.js
+++ src/index.js
@@ -112,11 +112,13 @@ async function drawPostDetail(postId) {
   const authorEl = frag.querySelector('.author')
   const bodyEl = frag.querySelector('.body')
   const backEl = frag.querySelector('.back')
+  const commentListEl = frag.querySelector('.comment-list')
 
   // 3. 필요한 데이터 불러오기
-  const {data: {title, body, user}} = await api.get('/posts/' + postId, {
+  const {data: {title, body, user, comments}} = await api.get('/posts/' + postId, {
     params: {
-      _expand: 'user'
+      _expand: 'user',
+      _embed: 'comments'
     }
   })
 
@@ -124,6 +126,24 @@ async function drawPostDetail(postId) {
   titleEl.textContent = title
   bodyEl.textContent = body
   authorEl.textContent = user.username
+  // 댓글 표시
+  for (const commentItem of comments) {
+    // 1. 템플릿 복사
+    const frag = document.importNode(templates.commentItem, true)
+
+    // 2. 요소 선택
+    const authorEl = frag.querySelector('.author')
+    const bodyEl = frag.querySelector('.body')
+    const deleteEl = frag.querySelector('.delete')
+
+    // 3. 필요한 데이터 불러오기 - 필요없음
+    // 4. 내용 채우기
+    bodyEl.textContent = commentItem.body
+
+    // 5. 이벤트 리스너 등록하기
+    // 6. 템플릿을 문서에 삽입
+    commentListEl.appendChild(frag)
+  }
 
   // 5. 이벤트 리스너 등록하기
   backEl.addEventListener('click', e => {
